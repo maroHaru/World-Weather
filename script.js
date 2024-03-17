4e7baf82a1312e1c696023edb0fe0c3c6cb72071
maroHaru <134841618+maroHaru@users.noreply.github.com>
2024-03-17 05:29:14 +0000

    [Finish] Implement Ajax

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
+const API_KEY = "578c5979bf8b0154157e87e87971c313";
+
+$(function(){
+  $('#btn').on('click', function() {
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data) {
+      alert('通信に失敗しました。');
+    });
   });
 });
